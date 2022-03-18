eb36bdf9e753190d61fa39cc8e72c46b4af2b6d2
hafez.smadhi <hafez.smadhi@aviation-civile.gouv.fr>
2022-03-18 18:19:25 +0100

    feat(cat063):transform cat063 and partially tested

--- transform/cat063_test.go
+++ transform/cat063_test.go
@@ -11,8 +11,12 @@ import (
 
 func TestCat063Model_ToJsonRecord(t *testing.T) {
 	// Arrange
-	input := "b0 0836 429b71 0801" //FSPEC : 10110000 => B0 f6 0836 429b71 0801 10940028200094008000
-	output := []byte(`{"dataSourceIdentifier":{"sac":8,"sic":54},"timeOfMessage":34102.8828125,"sensorIdentifier":{"sac":8,"sic":1}}`)
+	//bff0090c79387308290000000012000000000000000000000000
+	//input := "b0 0836 429b71 0801" //FSPEC : 10110000 => B0 f6 0836 429b71 0801 10940028200094008000
+
+	input := "bff0 090c 79387308290000000012000000000000000000000000"
+
+	output := []byte(`{"dataSourceIdentifier":{"sac":9,"sic":12},"timeOfMessage":62064.8984375,"sensorIdentifier":{"sac":8,"sic":41},"sensorConfigStatus":{"con":"operational","psr":"psr_go","ssr":"ssr_go","mds":"mode_s_go","ads":"ads_go","mlt":"mlt_go"},"timeStampingBias":0,"modeSRangeGainAndBias":{"srg":0.00018,"srb":0},"psrRangeGainAndBias":{"prg":0,"prb":0}}`)
 
 	uap063 := uap.Cat063V16
 	data, _ := goasterix.HexStringToByte(input)
